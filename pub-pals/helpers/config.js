28636077a4e0696471370b87df1a490f3f3f0580
rcpeters <info@rcpeters.com>
2016-11-03 05:01:49 -0700

    migrating to letsencrypt so we don't have to pay for SSL when we standup a live example

--- pub-pals/helpers/config.js
+++ pub-pals/helpers/config.js
@@ -19,9 +19,3 @@ module.exports = config = {
 for (key in config)
 	if (process.env[key] != undefined)
 		config[key] = process.env[key];
-
-// www.openshift.com deployment overrides
-if (process.env.OPENSHIFT_NODEJS_IP != undefined) 
-	config.SERVER_IP = process.env.OPENSHIFT_NODEJS_IP;
-if (process.env.OPENSHIFT_NODEJS_PORT != undefined) 
-	config.PORT = process.env.OPENSHIFT_NODEJS_PORT;
